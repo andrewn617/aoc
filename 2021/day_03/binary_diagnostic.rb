e50df76f7b1cd668e3d80086e4b1d5c8c2438e2b
Andrew Novoselac <andrew.novoselac@shopify.com>
2021-12-09 12:23:16 -0500

    Refactor

--- 2021/day_03/binary_diagnostic.rb
+++ 2021/day_03/binary_diagnostic.rb
@@ -4,22 +4,25 @@ class BinaryDiagnostic
   end
 
   def gamma
-    bit_matrix
-      .transpose
-      .map { |column| column.sum(&:to_i) > column.size / 2 ? 1 : 0 }
-      .join
-      .to_i(2)
+    most_occurences.to_i(2)
   end
 
   def epsilon
-    bit_matrix
-      .transpose
-      .map { |column| column.sum(&:to_i) > column.size / 2 ? 0 : 1 }
-      .join
-      .to_i(2)
+    least_occurences.to_i(2)
   end
 
   private
+
+  def most_occurences
+    @_most_common_bit ||= bit_matrix
+      .transpose
+      .map { |column| column.sum(&:to_i) > column.size / 2 ? 0 : 1 }
+      .join
+  end
+
+  def least_occurences
+    most_occurences.tr("01", "10")
+  end
 
   def bit_matrix
     @_bit_matrix ||= @input.split("\n").map(&:chars)
